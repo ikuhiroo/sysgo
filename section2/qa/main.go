14f8bb45721570ad168075b7cb1a81bb66631cb2
ikuhiroo <i2ku.nn@gmail.com>
2020-02-16 18:11:45 +0900

    2 done

--- section2/qa/main.go
+++ section2/qa/main.go
@@ -1,48 +1,51 @@
 // io.Writer は入出力に共通する仕様を満たすメソッドを持つ型
-// ファイルの入出力関連，バッファ，インターネットなど
+// https://www.write-ahead-log.net/entry/2017/08/06/025738
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
+	"compress/gzip"
+	"encoding/json"
+	"io"
 	"net/http"
 	"os"
-	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// 必要なHTTPヘッダを送信する
 	w.Header().Set("Content-Encoding", "gzip")
 	w.Header().Set("Content-Type", "application/json")
+
 	// JSON 化する元のデータ
 	source := map[string]string{
 		"Hello": "World",
 	}
+	gzipWriter := gzip.NewWriter(w)
+	defer gzipWriter.Close()
 
-	// http.ResponseWriter を指定して JSON エンコーダーで変換
-	// os.Stdout に出力するとログが残る
-	// io.MultiWriter
-	// JSON の文字列変換
-	// gzip 圧縮を行いながら圧縮前の出力を標準出力にも出す
+	// NewEncoder / Marshall / ...
+	// io.MultiWriter: gzipWriter を受け取り，書き込まれたデータを加工して，別の io.Writer(標準出力) に書き出す
+	encoder := json.NewEncoder(io.MultiWriter(gzipWriter, os.Stdout))
+	encoder.SetIndent("", "   ")
+	encoder.Encode(source)
 
+	// 圧縮せずに通信する
+	// bytes, _ := json.Marshal(source)
+	// io.WriteString(w, string(bytes))
 }
 
 func main() {
-	file, err := os.Create("qa_output.txt")
-	if err != nil {
-		panic(err)
-	}
+	// // Q2.1: フォーマットしてデータを io.Writer に書き出す
+	// fmt.Fprintf(file, "%d\n", time.Now())
 
-	// Q2.1: フォーマットしてデータを io.Writer に書き出す
-	fmt.Fprintf(file, "%d\n", time.Now())
-
-	// Q2.2: csv 出力 encoding/csv Flush() メソッドが必須
-	writer := csv.NewWriter(file)
-	writer.Write([]string{"Alice", "20"})
-	writer.Write([]string{"Bob", "21"})
-	writer.Write([]string{"Carol", "22"})
-	writer.Flush()
+	// // Q2.2: csv 出力 encoding/csv Flush() メソッドが必須
+	// writer := csv.NewWriter(file)
+	// writer.Write([]string{"Alice", "20"})
+	// writer.Write([]string{"Bob", "21"})
+	// writer.Write([]string{"Carol", "22"})
+	// writer.Flush()
 
 	// Q2.3: gzip された JSON 出力しながら，標準出力にログを出力
+	// 通信に使用するデータ量を制限する
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
